5cc4e9410b8605ccada0b1c5903e15e827e0a204
dewaqintoro <dewaqintoro@gmail.com>
2019-10-30 23:05:56 +0700

    telu

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,68 @@
-const CACHE_NAME = "firstpwa-v3.4.2";
+const CACHE_NAME = "pwa_3rr";
 var urlsToCache = [
 	"/",
-  "/nav.html",
   "/index.html",
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/contact.html",
+	"/nav.html",
+	"/pages/apple.html", 
+	"/pages/huawei.html",
+	"/pages/oppo.html",
+	"/pages/home.html",
+	"/pages/xiaomi.html",
   "/css/materialize.min.css",
   "/js/materialize.min.js",
 	"/js/nav.js",
-	"/icon.png"
+	"/img/Apple/apple-iphone-11-pro.jpg",
+	"/img/Apple/apple-ipad7-102-inches.jpg",
+	"/img/Apple/apple-watch-series-5.jpg",
+	"/img/Oppo/oppo-a5-2020.jpg",
+	"/img/Oppo/oppo-k5.jpg",
+	"/img/Oppo/oppo-reno-ace.jpg",
+	"/img/Huawei/huawei-enjoy-10s.jpg",
+	"/img/Huawei/huawei-mate-30-rs-porsche-design.jpg",
+	"/img/Huawei/huawei-nova-5z.jpg",
+	"/img/Xiaomi/xiaomi-redmi-8.jpg",
+	"/img/Xiaomi/xiaomi-mi-mix-alpha.jpg",
+	"/img/Xiaomi/xiaomi-mi-9-pro-5g.jpg"
+
 ];
 
-
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
 	);
-	
-self.addEventListener("fetch", function(event){
-	event.respondWith (
-		caches
-			.match(event.request, {cacheName : CACHE_NAME})
-			.then(function(response){
-				if (response){
-					console.log("ServiseWorker: gunakan aset dari cache : ", response.url);
-					return response;
-				}
-
-				console.log(
-					"ServiceWorker : Memuat aset dari cache: ",
-					event.request.url
-				);
-
-				return fetch(event.request)
-			})
-	)
 })
 
-self.addEventListener("activate", function(event){
+self.addEventListener('activate', function(event){
 	event.waitUntil(
-		caches.keys().then(function(cacheNames){
+		caches.keys()
+		.then(function(cacheNames) {
 			return Promise.all(
 				cacheNames.map(function(cacheName){
-					if (cacheName != CACHE_NAME){
-						console.log("ServiceWorker: cache " + cacheName + "dihapus")
-						return caches.delete(cacheName)
+					if(cacheName != CACHE_NAME){	
+						console.log("ServiceWorker: cache " + cacheName + " dihapus");
+						return caches.delete(cacheName);
 					}
 				})
-			)
+			);
 		})
-	)
+	);
 })
 
-
-
-
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request, {cacheName:CACHE_NAME})
+		.then(function(response) {
+			if(response){
+				console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+				return response;
+			}
+			
+			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+			return fetch(event.request);
+		})
+	);
 });
+
